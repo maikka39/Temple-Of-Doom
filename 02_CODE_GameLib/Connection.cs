9e4660cbf2215a545407ebf04d29fff99804fb9a
Maik de Kruif <maik@maik.dev>
2020-11-30 21:15:05 +0100

    Adds possibility to add an item at the connection location

--- 02_CODE_GameLib/Connection.cs
+++ 02_CODE_GameLib/Connection.cs
@@ -1,4 +1,5 @@
 ﻿using CODE_GameLib.Interfaces;
+using CODE_GameLib.Interfaces.Items;
 
 namespace CODE_GameLib
 {
@@ -6,11 +7,14 @@ namespace CODE_GameLib
     {
         public Room Destination { get; }
         public Location Location { get; }
+        
+        public IItem Item { get; }
 
-        public Connection(Room destination, Location location)
+        public Connection(Room destination, Location location, IItem item = null)
         {
             Destination = destination;
             Location = location;
+            Item = item;
         }
     }
 }
--- 02_CODE_GameLib/Interfaces/IConnection.cs
+++ 02_CODE_GameLib/Interfaces/IConnection.cs
@@ -1,8 +1,11 @@
+using CODE_GameLib.Interfaces.Items;
+
 namespace CODE_GameLib.Interfaces
 {
     public interface IConnection
     {
         public Room Destination { get; }
         public Location Location { get; } 
+        public IItem Item { get; }
     }
 }
