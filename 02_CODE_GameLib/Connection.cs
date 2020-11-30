5f9f0130fe76d6fbc4b3a4f8bb2695b0ebeb36c0
Maik de Kruif <maik@maik.dev>
2020-11-30 19:44:45 +0100

    Create a Connection model

---
+++ 02_CODE_GameLib/Connection.cs
@@ -0,0 +1,16 @@
+﻿using CODE_GameLib.Interfaces;
+
+namespace CODE_GameLib
+{
+    public class Connection : IConnection
+    {
+        public Room Destination { get; }
+        public Location Location { get; }
+
+        public Connection(Room destination, Location location)
+        {
+            Destination = destination;
+            Location = location;
+        }
+    }
+}
