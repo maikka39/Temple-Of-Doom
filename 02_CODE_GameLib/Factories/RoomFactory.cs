62631a87cb2e3c062ae4fdfcd457c4b597bd8444
Maik de Kruif <maik@maik.dev>
2020-12-13 19:15:36 +0100

    Add room validation

--- 02_CODE_GameLib/Factories/RoomFactory.cs
+++ 02_CODE_GameLib/Factories/RoomFactory.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using CODE_GameLib.Interfaces;
 using CODE_GameLib.Interfaces.Items;
@@ -8,22 +9,31 @@ namespace CODE_GameLib.Factories
 {
     public class RoomFactory
     {
-        public static IRoom CreateRoom(JObject roomJObject, IDictionary<int, List<IConnection>> connections, out int roomId)
+        public static IRoom CreateRoom(JObject roomJObject, IDictionary<int, List<IConnection>> connections,
+            out int roomId)
         {
             roomId = roomJObject["id"].Value<int>();
-            
+
             connections.Add(roomId, new List<IConnection>());
 
             var items = GetItemsForRoom(roomJObject);
 
-            return new Room(
-                roomJObject["width"].Value<int>(),
-                roomJObject["height"].Value<int>(),
-                items,
-                connections[roomId]
-            );
+            var width = roomJObject["width"].Value<int>();
+            var height = roomJObject["height"].Value<int>();
+
+            if (height < 3 || height > 50)
+                throw new ArgumentException("Height must be between 3 and 50");
+            if (width < 3 || width > 50)
+                throw new ArgumentException("Width must be between 3 and 50");
+
+            if (height % 2 == 0)
+                throw new ArgumentException("Height must be even");
+            if (width % 2 == 0)
+                throw new ArgumentException("Width must be even");
+
+            return new Room(width, height, items, connections[roomId]);
         }
-        
+
         private static List<IItem> GetItemsForRoom(JObject roomJObject)
         {
             var items = new List<IItem>();
