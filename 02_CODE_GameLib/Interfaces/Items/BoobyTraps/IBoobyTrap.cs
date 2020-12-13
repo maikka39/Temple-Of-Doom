d9c6d5889ff0fba1ec4b0bcea18745cadbe2e262
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2020-12-13 16:12:56 +0100

    Add DisapearingTrap

--- 02_CODE_GameLib/Interfaces/Items/BoobyTraps/IBoobyTrap.cs
+++ 02_CODE_GameLib/Interfaces/Items/BoobyTraps/IBoobyTrap.cs
@@ -1,8 +1,7 @@
-namespace CODE_GameLib.Interfaces.Items
+namespace CODE_GameLib.Interfaces.Items.BoobyTraps
 {
     public interface IBoobyTrap : IItem
     {
         public int Damage { get; }
-        public bool Disappearing { get;  }
     }
 }
--- 02_CODE_GameLib/Items/DisapearingTrap.cs
+++ 02_CODE_GameLib/Items/DisapearingTrap.cs
@@ -2,13 +2,13 @@ using CODE_GameLib.Interfaces.Items.BoobyTraps;
 
 namespace CODE_GameLib.Items
 {
-    public class BoobyTrap : IBoobyTrap
+    public class DisapearingTrap : IDisapearingTrap
     {
         public int X { get; }
         public int Y { get; }
         public int Damage { get; }
-
-        public BoobyTrap(int x, int y, int damage)
+        
+        public DisapearingTrap(int x, int y, int damage)
         {
             X = x;
             Y = y;
