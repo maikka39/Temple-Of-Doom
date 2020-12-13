dbe2132e7dd4ac7d2dcff21bd33cd627bcfe7a18
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2020-12-13 15:05:58 +0100

    Create Update Function

--- 02_CODE_GameLib/Interfaces/IGame.cs
+++ 02_CODE_GameLib/Interfaces/IGame.cs
@@ -6,6 +6,7 @@ namespace CODE_GameLib.Interfaces
     public interface IGame
     {
         public event EventHandler<Game> Updated;
+        public void Update();
         public bool Quit { get; }
         public IPlayer Player { get; }
         public IEnumerable<IRoom> Rooms { get; }
