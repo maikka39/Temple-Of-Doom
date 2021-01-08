33eef1e54ff643718776ffa9213576a04b80d4d6
Maik de Kruif <maik@maik.dev>
2021-01-08 21:05:26 +0100

    Remove doorviewmodel
    Doorviewmodel has been removed and it's logic has moved to
    ConnectionViewModel as cvm didn't do anything anymore

--- 01_CODE_Frontend/ViewModel/ConnectionViewModel.cs
+++ 01_CODE_Frontend/ViewModel/ConnectionViewModel.cs
@@ -1,4 +1,6 @@
-﻿using CODE_GameLib.Interfaces;
+﻿using CODE_GameLib.Enums;
+using CODE_GameLib.Interfaces;
+using CODE_GameLib.Interfaces.Doors;
 
 namespace CODE_Frontend.ViewModel
 {
@@ -8,11 +10,33 @@ namespace CODE_Frontend.ViewModel
 
         public int X => _connection.X;
         public int Y => _connection.Y;
-        public ConsoleText View => new DoorViewModel(_connection.Door, _connection.Direction).View;
+        public ConsoleText View => GetConnectionConsoleText(_connection);
         
         public ConnectionViewModel(IConnection connection)
         {
             _connection = connection;
+        }
+        
+        private static ConsoleText GetConnectionConsoleText(IConnection connection)
+        {
+            switch (connection.Door)
+            {
+                case IClosingDoor _:
+                    return new ConsoleText("⋂");
+                case IToggleDoor _:
+                    return new ConsoleText("⊥");
+                case IColoredDoor coloredDoor:
+                {
+                    var consoleText = new ConsoleText("|", Util.ColorToConsoleColor(coloredDoor.Color));
+                    
+                    if (connection.Direction == Direction.North || connection.Direction == Direction.South)
+                        consoleText.Text = "−";
+
+                    return consoleText;
+                }
+                default:
+                    return new ConsoleText(" ");
+            }
         }
     }
 }
